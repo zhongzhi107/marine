8fd9290a3ccfc8ab3b6c82477936afa88195fd6e
zhi.zhong <zhi.zhong@163.com>
2016-05-10 15:42:43 +0800

    Rename

--- src/middlewares/serverRender.js
+++ src/middlewares/serverRender.js
@@ -4,8 +4,8 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { match, RouterContext } from 'react-router';
 import template from 'lodash.template';
-import './src/utils/require';
-import routes from './src/main';
+import '../utils/require－polyfill';
+import routes from '../main';
 
 const META_KEYS = ['title', 'keywords', 'description', 'body'];
 
@@ -20,15 +20,17 @@ function render(data, res, renderProps, meta, templateString) {
 export default (req, res, next) => {
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
+      console.log('500');
       res.status(500).send(error.message);
     } else if (redirectLocation) {
+      console.log('302');
       res.redirect(302, redirectLocation.pathname + redirectLocation.search);
     } else if (renderProps) {
       // You can also check renderProps.components or renderProps.routes for
       // your "not found" component or route respectively, and send a 404 as
       // below, if you're using a catch-all route.
 
-      let filename = __dirname + '/src/public/index.html';
+      let filename = path.join(process.cwd(), 'src/public/index.html');
       let templateString = fs.readFileSync(filename, { encoding: 'utf8' });
       let meta = {
         pageInitialState: ''
@@ -41,8 +43,8 @@ export default (req, res, next) => {
       // 替换成混淆后的文件名
       // webpack-assets.json的数据如下：
       // {"main":{"js":"main.3148662cd2b83e1ae5f8.js"}}
-      let webpackAssets = './prd/webpack-assets.json';
-      if (fs.existsSync(path.join(process.cwd(), webpackAssets))) {
+      let webpackAssets = '../../prd/webpack-assets.json';
+      if (fs.existsSync(path.join(__dirname, webpackAssets))) {
         let stats = require(webpackAssets);
         for (let bundleName in stats) {
           for (let assetKind in stats[bundleName]) {
@@ -54,27 +56,33 @@ export default (req, res, next) => {
       }
 
       // 取最下层的组件作为目标组件
-      let Component = renderProps.components[renderProps.components.length - 1];
+      let {components, params} = renderProps;
+      let Component = components[components.length - 1];
       if (Component) {
         // 将Component的静态属性赋给meta
         META_KEYS.forEach((key) => {
           if (Component.hasOwnProperty(key)) {
-            meta[key] = template(Component[key])(renderProps.params);
+            meta[key] = template(Component[key])(params);
           }
         });
 
         if (Component.observe) {
-          Component
-            .observe()
-            .then((data) => {
-              render(data, res, renderProps, meta, templateString);
-            }, (reason) => {
-              render(Component.defaultState, res, renderProps, meta, templateString);
-            })
-            .catch(() => {
-              // 接口出现问题
-              render(Component.defaultState, res, renderProps, meta, templateString);
-            });
+          try {
+            Component
+              .observe()
+              .then((data) => {
+                render(data, res, renderProps, meta, templateString);
+              }, (reason) => {
+                render(Component.defaultState, res, renderProps, meta, templateString);
+              })
+              .catch(() => {
+                // 接口出现问题
+                render(Component.defaultState, res, renderProps, meta, templateString);
+              });
+          } catch (e) {
+            render(Component.defaultState, res, renderProps, meta, templateString);
+          }
+
         } else {
           render(Component.defaultState, res, renderProps, meta, templateString);
         }
