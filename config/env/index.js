ae25e249a1d8be562267cfa218b5d6fe2b1f9488
zhongzhi107 <zhi.zhong@163.com>
2016-05-12 23:01:05 +0800

    删除beta和local环境类型

--- config/env/index.js
+++ config/env/index.js
@@ -1,13 +1,9 @@
 'use strict';
 
 import development from './development';
-import beta from './beta';
-import local from './local';
 import production from './production';
 
 export default {
-  beta,
   development,
-  local,
   production,
 };
