d8b5f1ae758b56148e1663c954d54c273395b94f
zhongzhi107 <zhong.zhi@163.com>
2016-06-10 23:25:55 +0800

    删除bootstrap

--- src/helpers/Html.js
+++ src/helpers/Html.js
@@ -25,7 +25,7 @@ export default class Html extends Component {
     const head = Helmet.rewind();
 
     return (
-      <html lang="en-us">
+      <html lang="zh">
         <head>
           {head.base.toComponent()}
           {head.title.toComponent()}
@@ -45,7 +45,7 @@ export default class Html extends Component {
           {/* outputs a <style/> tag with all bootstrap styles + App.scss + it could be CurrentPage.scss. */}
           {/* can smoothen the initial style flash (flicker) on page load in development mode. */}
           {/* ideally one could also include here the style for the current page (Home.scss, About.scss, etc) */}
-          { Object.keys(assets.styles).length === 0 ? <style dangerouslySetInnerHTML={{__html: require('../theme/bootstrap.config.js') + require('../containers/App/App.scss')._style}}/> : null }
+          { Object.keys(assets.styles).length === 0 ? <style dangerouslySetInnerHTML={{__html: ''}}/> : null }
         </head>
         <body>
           <div id="content" dangerouslySetInnerHTML={{__html: content}}/>
